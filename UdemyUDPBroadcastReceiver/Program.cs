cba271fcca0dbf97d876c83aaa59c6800f65f664
MiyuruCG <miyurugunatilake@gmail.com>
2020-03-28 14:17:57 +0530

    Modified so the received byte[] in converted to string V1

--- UdemyUDPBroadcastReceiver/Program.cs
+++ UdemyUDPBroadcastReceiver/Program.cs
@@ -17,7 +17,7 @@ namespace UdemyUDPBroadcastReceiver
         static void Main(string[] args)
         {
             Socket sockBroadcastReceiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            //
+            //...
 
             IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 23000); // ip address and port no which we will recive the data
             //1st parameter :: Receives data from all available IP adresses from this host 
@@ -27,6 +27,9 @@ namespace UdemyUDPBroadcastReceiver
             byte[] receiverBuffer = new byte[512];
             int countReceived = 0;
 
+            //coverting received byte[] data to text string so the receiver can print the string on the console 
+            string txtReceived = string.Empty;
+
             try
             {
                 sockBroadcastReceiver.Bind(ipEndPoint); // binds the endpoint to the reciving socket
@@ -34,7 +37,13 @@ namespace UdemyUDPBroadcastReceiver
                 {
                     //receive methord
                     countReceived = sockBroadcastReceiver.Receive(receiverBuffer);
+                    txtReceived = Encoding.ASCII.GetString(receiverBuffer, 0, countReceived);
+                    //1 :: the received buffer
+                    //2 :: the starting point index of the getString operation
+                    //3 :: no of bytes we need to convert in to text
+
                     Console.WriteLine("Number of byted recieved : " + countReceived);
+                    Console.WriteLine(" Received :: "+ txtReceived);
                     Array.Clear(receiverBuffer, 0 , receiverBuffer.Length); 
                     // cause using the same array to get the data again ans again needs to clear it before adding 
                 }
